f9c5eeb04086919d49e104a44ce1a31d62163688
Siwirus <kotyukov.iv@edu.spbstu.ru>
2023-04-30 19:00:43 +0300

    task1

--- js/main.js
+++ js/main.js
@@ -3,3 +3,23 @@ function getRandomNumber(a, b) {
 }
 const stringLength = (str, maxLenght) => str.length <= maxLenght;
 
+function randomObject(a) {
+  return {
+    id: a + 1,
+    url: a + 1,
+    description: 'Великолепно',
+    likes: getRandomNumber(15, 2000),
+    comments: getRandomNumber(0, 200),
+  };
+}
+function randomDescription(){
+  let photos = [];
+  for (let i = 0; i <= 24; i++){
+    const object = randomObject(i);
+    photos[i] = object;
+  }
+  return photos;
+}
+
+
+console.log(randomDescription());
