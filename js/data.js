b71d7f12039ed35d864e9f38e5131dfe8482b1cf
Siwirus <kotyukov.iv@edu.spbstu.ru>
2023-05-13 21:18:06 +0300

    module11

--- js/data.js
+++ js/data.js
@@ -1,24 +1,3 @@
-import {getRandomNumber} from './util.js';
-function randomObject(a) {
-  return {
-    id: a + 1,
-    url: `photos/${a + 1}.jpg`,
-    description: 'Великолепно',
-    likes: getRandomNumber(15, 2000),
-    comments: getRandomNumber(0, 200),
-  };
-}
-function randomDescription(){
-  const photos = [];
-  for (let i = 0; i <= 24; i++){
-    const object = randomObject(i);
-    photos[i] = object;
-  }
-  return photos;
-}
-
-
-//console.log(randomDescription());
 const sepia = {
   name: 'sepia',
   filter: 'sepia',
